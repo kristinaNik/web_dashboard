1eee6ee33222224efcad97e13b1428e02af2c8cc
kristinaNik <kristina.nik93@gmail.com>
2020-02-18 11:29:52 +0200

    Added new view for form and added a controller

---
+++ app/Http/Controllers/WebDashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebDashboardController extends Controller
+{
+
+
+    public function addConfigurations(Request $request)
+    {
+
+    }
+}
---
+++ resources/views/form.blade.php
@@ -0,0 +1,20 @@
+<form>
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input type="title" class="form-control" id="title">
+    </div>
+    <div class="form-group">
+        <label for="link">Link</label>
+        <input type="link" class="form-control" id="link">
+    </div>
+    <div class="form-group">
+
+        <label for="color">Color</label>
+        <select class="form-control" id="color">
+
+            <option value="">Choose</option>
+
+        </select>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
